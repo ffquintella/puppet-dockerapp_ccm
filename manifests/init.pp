f377c31a8512980d85c675e63eaa0c81071c99c2
Felipe F Quintella <felipe@quintella.com>
2019-03-22 12:52:30 -0300

    TESTS

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,18 +11,89 @@
 # [*ports*]
 #   Ports to be used by the app
 #
+# [*redis_server*]
+#   The fqnd of the redis server 
+#
+# [*redis_port*] (optional)
+#   The tcp port of the redis server
+#
+# [*redis_database*] (optional)
+#   The database to use in redis
+#
 # [*masterkey*]
 #   The encryption masterkey
 #
 # [*service_name*] (optional)
 #   The name to be used for the service and directories 
 #
+# [*smpt_server*] 
+#   The server to send e-mails  
+#
+# [*email_from*] 
+#   The email to use on the from field
+#
+# [*email_name*] 
+#   The name to appear on the e-mails
+#
+# [*timezone*] (optional)
+#   The php timezone
+#
+# [*log_level*] (optional)
+#   The log verbosity INFO|DEBUG
+#
+# [*session_time*] (optional)
+#   The session time in seconds
+#
+# [*http_timeout*] (optional)
+#   The http timeout in seconds
+#
+# [*php_timeout*] (optional)
+#   The php timeout in seconds
+#
+# [*ssl_required*] (optional)
+#   If ssl is required. Only disable this in development environment
+#
+# [*auth_required*] (optional)
+#   If authentication is required. Only disable this in development environment
+#
+# [*passwd_size*] (optional)
+#   The size of the generated passwords
+#
+# [*user_passwd_size*] (optional)
+#   The size of the user required password
+#
+# [*app_key_size*] (optional)
+#   The size of the app key
+#
+# [*cache_timeout*] (optional)
+#   The time in seconds to keep the cached data
+#
+# [*cache_dns_timeout*] (optional)
+#   The time in seconds to keep the cached dns results
+#
 class dockerapp_ccm  (
   $version = '1.5.51',
-  $ports = [ '4443:443'],
+  $ports = ['4443:443'],
   $redis_server = undef,
+  $redis_port = '6379',
+  $redis_database = '1',
   $masterkey = undef,
-  $service_name = 'ccm'
+  $service_name = 'ccm', 
+  $smpt_server = undef,
+  $email_from = undef, 
+  $email_name = undef,
+  $timezone = 'America/Sao_Paulo', 
+  $log_level = 'INFO',
+  $session_time = '600',
+  $http_timeout = '15',
+  $php_timeout = '300',
+  $ssl_required = 'true',
+  $auth_required = 'true',
+  $passwd_size = '25',
+  $user_passwd_size = '15',
+  $app_key_size = '32',
+  $cache_timeout = '1200',
+  $cache_dns_timeout = '600',
   ){
 
   include 'dockerapp'
@@ -80,24 +151,24 @@ class dockerapp_ccm  (
   ]
 
   $envs = [
-    'FACTER_TIMEZONE=America/Sao_Paulo',
-    'FACTER_LOG_LEVEL=DEBUG',
-    'FACTER_SESSION_TIME=600',
-    'FACTER_SMTP_SERVER=smtp.ccb.com',
-    'FACTER_EMAIL_FROM=ccm@abc.com',
-    'FACTER_EMAIL_FROM_NAME=CCM SERVER',
-    'FACTER_HTTP_TIMEOUT=15',
-    'FACTER_PHP_TIMEOUT=300',
-    'FACTER_HTTPS_REQUIRED=true',
-    'FACTER_AUTHENTICATION_REQUIRED=true',
-    'FACTER_PASS_SIZE=25',
-    'FACTER_USER_PASS_SIZE=15',
-    'FACTER_APP_KEY_SIZE=32',
-    'FACTER_CACHE_TIMEOUT=1200',
-    'FACTER_CACHE_DNS_TIMEOUT=600',
+    "FACTER_TIMEZONE=${timezone}",
+    "FACTER_LOG_LEVEL=${log_level}",
+    "FACTER_SESSION_TIME=${session_time}",
+    "FACTER_SMTP_SERVER=${smpt_server}",
+    "FACTER_EMAIL_FROM=${email_from}",
+    "FACTER_EMAIL_FROM_NAME=${email_name}",
+    "FACTER_HTTP_TIMEOUT=${http_timeout}",
+    "FACTER_PHP_TIMEOUT=${php_timeout}",
+    "FACTER_HTTPS_REQUIRED=${ssl_required}",
+    "FACTER_AUTHENTICATION_REQUIRED=${auth_required}",
+    "FACTER_PASS_SIZE=${passwd_size}",
+    "FACTER_USER_PASS_SIZE=${user_passwd_size}",
+    "FACTER_APP_KEY_SIZE=${app_key_size}",
+    "FACTER_CACHE_TIMEOUT=${cache_timeout}",
+    "FACTER_CACHE_DNS_TIMEOUT=${cache_dns_timeout}",
     "FACTER_REDIS_SERVER=${redis_server}",
-    'FACTER_REDIS_PORT=6379',
-    'FACTER_REDIS_DATABASE=1',
+    "FACTER_REDIS_PORT=${redis_port}",
+    "FACTER_REDIS_DATABASE=${redis_database}",
   ]
 
   dockerapp::run {$service_name:
--- spec/classes/dockerapp_ccm_spec.rb
+++ spec/classes/dockerapp_ccm_spec.rb
@@ -7,7 +7,11 @@ describe '::dockerapp_ccm' do
       version: '1.4.1',
       redis_server: 'redis-123.com',
       masterkey: 'abcv1263547563',
-      ports: ['8443:443']
+      ports: ['8443:443'],
+      log_level: 'DEBUG',
+      smpt_server: 'smtp1',
+      email_from: 'test@mail.com',
+      email_name: 'testMail'
     }
   end
 
@@ -45,7 +49,33 @@ describe '::dockerapp_ccm' do
 
 
   it { is_expected.to contain_dockerapp__run('ccm').with(
-    'image' => 'ffquintella/ccm:1.4.1',
-    'ports' => ['8443:443'],
+    'image'   => 'ffquintella/ccm:1.4.1',
+    'ports'   => ['8443:443'],
+    'volumes' => [
+      '/srv/application-log/ccm/nginx:/var/log/nginx',
+      '/srv/application-log/ccm/php-fpm:/var/log/php-fpm',
+      '/srv/application-log/ccm/ccm:/var/log/ccm',
+      '/srv/application-config/ccm/masterkey.php:/app/masterkey.php',
+    ],
+    'environments' => [
+      'FACTER_TIMEZONE=America/Sao_Paulo',
+      'FACTER_LOG_LEVEL=DEBUG',
+      'FACTER_SESSION_TIME=600',
+      'FACTER_SMTP_SERVER=smtp1',
+      'FACTER_EMAIL_FROM=test@mail.com',
+      'FACTER_EMAIL_FROM_NAME=testMail',
+      'FACTER_HTTP_TIMEOUT=15',
+      'FACTER_PHP_TIMEOUT=300',
+      'FACTER_HTTPS_REQUIRED=true',
+      'FACTER_AUTHENTICATION_REQUIRED=true',
+      'FACTER_PASS_SIZE=25',
+      'FACTER_USER_PASS_SIZE=15',
+      'FACTER_APP_KEY_SIZE=32',
+      'FACTER_CACHE_TIMEOUT=1200',
+      'FACTER_CACHE_DNS_TIMEOUT=600',
+      'FACTER_REDIS_SERVER=redis-123.com',
+      'FACTER_REDIS_PORT=6379',
+      'FACTER_REDIS_DATABASE=1',
+    ]
     ) }
 end
